77ac8e8d0a292e9daa68fbc1aeb9ca9d98db6fd5
dongmiii <djys1004@gmail.com>
2024-12-01 17:00:10 +0900

    modify memberEntity

--- finalProject/src/main/java/com/example/finalProject/controller/LifeController.java
+++ finalProject/src/main/java/com/example/finalProject/controller/LifeController.java
@@ -1,7 +1,7 @@
 package com.example.finalProject.controller;
 
-import com.example.finalProject.domain.entity.News2Entity;
-import com.example.finalProject.service.News2Service;
+import com.example.finalProject.domain.entity.LifeEntity;
+import com.example.finalProject.service.LifeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class News2Controller {
+public class LifeController {
 
-    private final News2Service news2Service;
+    private final LifeService news2Service;
 
-    public News2Controller(News2Service newsService) {
+    public LifeController(LifeService newsService) {
         this.news2Service = newsService;
     }
 
     // 기존 news 요청
-    @GetMapping("/news2")
+    @GetMapping("/life")
     public String showNews(Model model) {
-        List<News2Entity> newsList = news2Service.getAllNews();
+        List<LifeEntity> newsList = news2Service.getAllNews();
         model.addAttribute("newsList", newsList);
-        return "news";
+        return "/news/life";
     }
 }
--- finalProject/src/main/java/com/example/finalProject/domain/repository/LifeRepository.java
+++ finalProject/src/main/java/com/example/finalProject/domain/repository/LifeRepository.java
@@ -1,6 +1,6 @@
 package com.example.finalProject.domain.repository;
 
-import com.example.finalProject.domain.entity.News2Entity;
+import com.example.finalProject.domain.entity.LifeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface News2Repository extends JpaRepository<News2Entity, Long> {
+public interface LifeRepository extends JpaRepository<LifeEntity, Long> {
 
     @Query(value = "SELECT DISTINCT title, url, summary, press_source FROM life_news_articles ORDER BY id DESC LIMIT 3", nativeQuery = true)
-    List<News2Entity> findLatestThree();
+    List<LifeEntity> findLatestThree();
 }
